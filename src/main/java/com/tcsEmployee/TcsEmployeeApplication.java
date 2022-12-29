9a05077b94358b82b892f098086cb3fc4ea65f78
java_ranganath <ranganath8696@gamil.com>
2022-12-29 22:22:49 +0530

    added thank you

---
+++ src/main/java/com/tcsEmployee/TcsEmployeeApplication.java
@@ -0,0 +1,13 @@
+package com.tcsEmployee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcsEmployeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TcsEmployeeApplication.class, args);
+	}
+
+}
